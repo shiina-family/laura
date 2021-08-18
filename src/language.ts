705120d954f7f03aea67f9a76c8a850d1db251e3
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-08-18 21:09:10 +0900

    add language.ts

---
+++ src/language.ts
@@ -0,0 +1,3 @@
+export const solveFifteenPuzzle = '**SOLVE 15 PUZZLE**';
+export const loggedInAs = (username: string) => `Logged in as ${username}.`;
+export const loadedCog = (cogStem: string) => `Loaded ${cogStem}.`;
